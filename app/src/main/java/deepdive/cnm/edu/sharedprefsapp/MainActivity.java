670a675ed86a617164882ec6d9b290214fee0b98
ToraKaji <ogradysjr@gmail.com>
2018-11-13 13:45:44 -0600

    Initial commit

---
+++ app/src/main/java/deepdive/cnm/edu/sharedprefsapp/MainActivity.java
@@ -0,0 +1,50 @@
+package deepdive.cnm.edu.sharedprefsapp;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+  @BindView(R.id.data_button)
+  Button dataButton;
+
+  @BindView(R.id.data_text_out)
+  TextView dataOutText;
+
+  @BindView(R.id.data_input)
+  EditText dataInput;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
+
+    dataButton.setOnClickListener((view) -> {
+      saveToSharedPrefs(dataInput.getText().toString());
+      dataOutText.setText(getFromSharedPrefs());
+    });
+
+    dataOutText.setText(getFromSharedPrefs());
+  }
+
+  private void saveToSharedPrefs(String s){
+    SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+    SharedPreferences.Editor editor = sharedPreferences.edit();
+    editor.putString(getString(R.string.string_key), s);
+    editor.putInt("FOO LENGTH",s.length());
+    editor.apply();
+  }
+
+  private String getFromSharedPrefs(){
+    SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+    return sharedPreferences.getString(getString(R.string.string_key), "");
+  }
+}
